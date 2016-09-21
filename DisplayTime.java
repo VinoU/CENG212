bb14e98bfeb50a9fe5eb93ecd2b23fb77b17b43b
VinoU <ukvino3@gmail.com>
2016-09-21 14:02:16 -0400

    Create DisplayTime.java

---
+++ DisplayTime.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+publicclass DisplayTime {
+publicstaticvoid main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter an integer for seconds: "); 
+int seconds = input.nextInt();
+int minutes = ;
+int remainingSeconds = ; // Seconds remaining
+System.out.println(seconds + " seconds is " + minutes + " minutes and " + remainingSeconds + " seconds");
+}
+}
